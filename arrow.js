68b35f539e64a3e1ffd01b3213f32abaca4b2b31
mehedi345 <mehedihasan5101220@gmail.com>
2020-08-06 20:56:50 -0700

    es6 arrow function

---
+++ arrow.js
@@ -0,0 +1,14 @@
+// function doubleIt(num){
+//     return num * 2 ;
+
+// }
+
+// const doubleIt = function(num){
+
+//     return num * 2;
+// }
+const doubleIt = num => num * 2 ;
+const add = (x, y) => x + y ;
+const result2 = add(30, 40);
+const result = doubleIt(5);
+console.log(result2);
