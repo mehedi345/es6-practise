decbca7b4c92f641b6564846479f0ac942aa6e8c
mehedi345 <mehedihasan5101220@gmail.com>
2020-08-08 14:23:28 -0700

    commit for es6

---
+++ scope.js
@@ -0,0 +1,10 @@
+
+let bonus = 20;
+
+function sum (first, second){
+    let resut = first + second + bonus;
+    console.log(resut);
+    return resut;
+}
+const output = sum(3, 5);
+console.log(output);
