2a28e92f0988df493f526126bf1e33ca6524a0bb
mehedi345 <mehedihasan5101220@gmail.com>
2020-08-13 19:38:47 -0700

    practicing double-triple

---
+++ double-vs-triple.js
@@ -0,0 +1,17 @@
+const first = 2;
+const second = "2";
+if(first === second){
+    console.log("condition is true");
+} else{
+    console.log("condition is false");
+} 
+
+//double equal depend on value don't data-type but triple equal depend on value and data-type
+
+const third = 0;
+const fourth = true;
+if(first == second){
+    console.log("condition is true");
+} else{
+    console.log("condition is false");
+} 
