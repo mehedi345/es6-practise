930de1d3d5554be423fbb869c79a8995f7cd2cbe
mehedi345 <mehedihasan5101220@gmail.com>
2020-08-13 19:28:41 -0700

    practising null-undefined and truthy-falsy

---
+++ null-undefined.js
@@ -0,0 +1,17 @@
+let name;//1 - not value assigned 
+function add (num1, num2){
+    console.log(num1 + num2); // 2 - function without return , nothing to return. don't pass parameter.
+}
+const  result = add(4, 7);
+console.log(result);
+
+const lover = {
+    name : "smart dude",
+    phone: 90890
+}
+const gf = lover.gf;
+//console.log(gf); 
+// jodi kono object e kono property na thake kintu ta output hisebe nite chaile tokhon undefined .
+//variable er value undefined set korle.
+
+//NULL
---
+++ true-false.js
@@ -0,0 +1,11 @@
+const age = 5;
+let name;
+if(age){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+//FALSY- VALUES: 0, "", undefined, null, NaN, false. 
+//truthy- "0", " ", [], {}, anything else accept falsy values.
