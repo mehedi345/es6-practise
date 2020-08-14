d3aef287916aaf50a53669a28b474fa0501d7143
mehedi345 <mehedihasan5101220@gmail.com>
2020-08-13 20:10:59 -0700

    practicing map and filter

--- map-filter.js
+++ map-filter.js
@@ -5,12 +5,17 @@ const numbers = [3, 4, 5, 6, 7, 8];
 //     const square = element * element;
 //     output.push(square);
 // }
-// const result = numbers.map(function (element){
+// console.log(output);
+const result = numbers.map(function (element){
     
-//     return element * element;
-// })
-// const result = numbers.map(x => x * x);
-// console.log(result);
+    return element * element;
+})
+const result2 = numbers.map(x => x * x);
+console.log(result2);
 
 const smaller = numbers.find(x => x < 5);
 console.log(smaller);
+
+const result3 = numbers.filter(x => x < 5);
+console.log(result3);
+
--- objectProperty.js
+++ objectProperty.js
@@ -4,13 +4,20 @@ const students = [
     {id:41, name : "mahiya mahi"},
     {id:71, name : "deepjol"}
 ]
-// for(let i = 0; i<students.length, i++){
-//     const element = students[i];
-    
-// }
+const output = [];
+for(let i = 0; i<students.length; i++){
+    const element = students[i];
+    const name = element.name;
+    output.push(name);
+}
+console.log(output);
+
+
 const names = students.map(s => s.name);
 console.log(names);
 const ids = students.map(s => s.id);
 console.log(ids);
 const bigger = students.filter(s => s.id >40);
 console.log(bigger);
+const bigger1 = students.find(s => s.id >40);
+console.log(bigger1);
