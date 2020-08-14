63686c5367157ac73d6dee37784b3d7c14bc62bd
mehedi345 <mehedihasan5101220@gmail.com>
2020-08-13 20:25:26 -0700

    practicing variable scope

--- scope.js
+++ scope.js
@@ -1,10 +1,10 @@
 
-let bonus = 20;
+let bonus = 20; // global scope
 
 function sum (first, second){
-    let resut = first + second + bonus;
-    console.log(resut);
-    return resut;
+    let result = first + second + bonus;
+    //console.log(result); local scope
+    return result;
 }
 const output = sum(3, 5);
 console.log(output);
